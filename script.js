4fefa8449824bbbb5c87d2635ec638ff147e8d17
tohko <j2x.al.nh@hotmail.co.jp>
2020-05-10 12:51:47 +0100

    may apdate

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
 $(function(){
-  
+  $('.page-list-item').hover(
+    function(){
+      $(this).find('a').css('color','#333')
+
+    },
+    function(){
+      $(this).find('a').css('color','white')
+    }
+  );
+
+  $('.travel-link').hover(
+    function(){
+      $('.travelpic' , this).fadeIn()
+      $(this).css('width','200%')
+      $('a' , this).css('color','rgba(255, 255, 255, 1)')
+    },
+    function(){
+      $('.travelpic' , this).fadeOut()
+      $(this).css('width','100%')
+      $('a' , this).css('color','#333')
+    }
+  );
+
 })
 
